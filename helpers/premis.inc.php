889908dfcb8d0bbc59cbaaff2f86a79182590d2b
Tessa Walsh <tessa@bitarchivist.net>
2022-06-28 16:52:55 -0400

    Modify PREMIS helpers

--- helpers/premis.inc.php
+++ helpers/premis.inc.php
@@ -14,18 +14,29 @@ function getStatueCitationId()
     return $id;
 }
 
+function getActId($nameText='Disseminate')
+{
+  $sql = "SELECT id from term_i18n WHERE name=? AND culture='en'";
+
+  $id = QubitPdo::fetchColumn($sql, [$nameText]);
+
+  if ($id === false)
+  {
+    print "Act ". $nameText ." does not exist in AtoM database.\n";
+    exit;
+  }
+
+  return $id;
+}
+
+function getDisplayActId()
+{
+    return getActId('Display');
+}
+
 function getDisseminateActId()
 {
-    $sql = "SELECT id from term_i18n WHERE name=? AND culture='en'";
-
-    $id = QubitPdo::fetchColumn($sql, ["Disseminate"]);
-
-    if ($id === false) {
-        print "Run this after create_container_note_type_and_premis_statute.php\n";
-        exit;
-    }
-
-    return $id;
+    return getActId('Disseminate');
 }
 
 function getUnderCopyrightId()
@@ -42,32 +53,59 @@ function getUnderCopyrightId()
     return $id;
 }
 
-function addRights($objectId, $copyrightStatusId, $citationId, $actId, $restriction, $basisId = null)
+function addOrGetRights($objectId, $basisId = null)
 {
-    if (empty($basisId)) {
-        $basisId = QubitTerm::RIGHT_BASIS_POLICY_ID;
+  // Return existing rights if already linked to object
+  $criteria = new Criteria;
+  $criteria->add(QubitRelation::SUBJECT_ID, $objectId);
+  $criteria->add(QubitRelation::TYPE_ID, QubitTerm::RIGHT_ID);
+  if (null !== $relation = QubitRelation::getOne($criteria))
+  {
+    $rightsId = $relation->objectId;
+    $criteria = new Criteria;
+    $criteria->add(QubitRights::ID, $rightsId);
+    if (null !== $rights = QubitRights::getOne($criteria))
+    {
+      return $rights;
     }
+  }
 
-    $rights = new QubitRights();
-    $rights->objectId = $objectId;
-    $rights->basisId = $basisId;
-    $rights->copyrightStatusId = $copyrightStatusId;
+  if (empty($basisId))
+  {
+    $basisId = QubitTerm::RIGHT_BASIS_POLICY_ID;
+  }
 
-    if ($basisId == QubitTerm::RIGHT_BASIS_STATUTE_ID) {
-        $rights->statuteCitationId = $citationId;
-    }
+  $rights = new QubitRights;
+  $rights->objectId = $objectId;
+  $rights->basisId = $basisId;
+  $rights->save();
 
-    $rights->save();
+  $rel = new QubitRelation;
+  $rel->subjectId = $objectId;
+  $rel->objectId = $rights->id;
+  $rel->typeId = QubitTerm::RIGHT_ID;
+  $rel->save();
 
-    $granted = new QubitGrantedRight();
-    $granted->rightsId = $rights->id;
-    $granted->actId = $actId;
-    $granted->restriction = $restriction;
-    $granted->save();
+  return $rights;
+}
 
-    $rel = new QubitRelation();
-    $rel->subjectId = $objectId;
-    $rel->objectId = $rights->id;
-    $rel->typeId = QubitTerm::RIGHT_ID;
-    $rel->save();
+function addRightsNotes($rights, $rightsNote, $copyrightNote)
+{
+  $rights->rightsNote = $rightsNote;
+  $rights->copyrightNote = $copyrightNote;
+  $rights->save();
+}
+
+function addGrantedRight($rights, $actId, $restriction, $endDate, $note)
+{
+  $granted = new QubitGrantedRight;
+  $granted->rightsId = $rights->id;
+  $granted->actId = $actId;
+  $granted->restriction = $restriction;
+  $granted->endDate = $endDate;
+  $granted->note = $note;
+  $granted->save();
+
+  $rights->grantedRights[] = $granted;
+  $rights->save();
 }
